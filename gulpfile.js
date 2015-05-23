6d2dc8a7126bed691fbeae5067d9d9dbf37b70cb
th3mon <p.szelenberger@gmail.com>
2015-05-23 21:00:04 +0200

    Refactoring: separated tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,41 @@
 var
   gulp = require('gulp'),
   connect = require('gulp-connect'),
-  less = require('gulp-less');
+  less = require('gulp-less'),
+  watch = require('gulp-watch'),
+  coffee = require('gulp-coffee');
 
 gulp.task('webserver', function () {
   connect.server({
-    livereload: true
+    livereload: true,
+    root: ['.', '.tmp']
   });
 });
 
-gulp.task('less', function () {
-  gulp.src('styles/main.less')
-    .pipe(less())
-    .pipe(gulp.dest('styles'))
+gulp.task('livereload', function() {
+  gulp
+    .src(['.tmp/styles/*.css', '.tmp/scripts/*.js'])
+    .pipe(watch())
     .pipe(connect.reload());
+});
+
+gulp.task('less', function () {
+  gulp
+    .src('styles/main.less')
+    .pipe(less())
+    .pipe(gulp.dest('.tmp/styles'));
+});
+
+gulp.task('coffee', function() {
+  gulp
+    .src('scripts/*.coffee')
+    .pipe(coffee())
+    .pipe(gulp.dest('.tmp/scripts'));
 });
 
 gulp.task('watch', function () {
   gulp.watch('styles/*.less', ['less']);
+  gulp.watch('scripts/*.coffee', ['coffee']);
 });
 
-gulp.task('default', ['less', 'webserver', 'watch']);
+gulp.task('default', ['less', 'coffee', 'webserver', 'livereload', 'watch']);
---
+++ scripts/main.coffee
@@ -0,0 +1,4 @@
+h1 = document.createElement("h1")
+text = document.createTextNode("Kocham mojego Kotka :-*")
+h1.appendChild text
+document.body.appendChild h1
